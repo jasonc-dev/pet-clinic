191d698d5178703e46f468513181f71e06f71013
jasonc-dev <jasoncohen07@gmail.com>
2020-10-19 17:41:42 +0100

    new commit

--- pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicApplicationTests.java
@@ -5,14 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class PetClinicApplicationTest {
-
+class PetClinicApplicationTests {
     @Test
     public void contextLoads() {
 
     }
-
 }
